e8ba8ca1c40a14493cbcf9b9e6ec8036e382e91d
kaush <emailtokaush@gmail.com>
2019-11-14 19:00:52 -0500

    modification added

--- meteor-project/social-media/client/main.js
+++ meteor-project/social-media/client/main.js
@@ -1,22 +1,49 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import { Mongo } from 'meteor/mongo';
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+Post = new Mongo.Collection('post')
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.postList.helpers({
+  posts :function() {
+    return Post.find({},{sort:{created: -1}});
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Template.postForm.events({
+  'submit form' :function(event) {
+    event.preventDefault();
+    var content = document.getElementById('content').value;
+
+    //Meteor.call('addPost', content)
+    var username = Meteor.user().username;
+
+    Post.insert({content: content, created: new Date(),username : username});
+    event.target.reset();
   },
 });
+
+
+Accounts.ui.config({
+  requestPermissions: {
+    facebook: ['user_likes'],
+    github: ['user', 'repo']
+  },
+  requestOfflineToken: {
+    google: true
+  },
+  // passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+  passwordSignupFields: 'USERNAME_ONLY'
+});
+
+// Meteor.methods({
+//   addPost : function(content){
+//     if(!Meteor.userId()){
+//       throw new Meteor.Error('not-authorized', 'you are not signed in')
+//     }
+//     var username = Meteor.user().username;
+//
+//     Post.insert({content: content, created: new Date(),username : username});
+//   }
+// })
